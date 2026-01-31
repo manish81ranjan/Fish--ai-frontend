17a71c533183dc66e9fe17a2658bfe92dad793de
Manish-2410 <vkdeoghar81@gmail.com>
2026-01-31 22:36:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -148,10 +148,11 @@ def accessories():
 
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
+
 
 
