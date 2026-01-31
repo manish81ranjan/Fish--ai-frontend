e020348f91e2b36bdbf3c6239dc52ea40f404ea1
Manish-2410 <vkdeoghar81@gmail.com>
2026-01-31 22:23:41 +0530

    Modify app.run to use dynamic port configuration
    Update the main application to run on a dynamic port.

--- backend/main.py
+++ backend/main.py
@@ -148,5 +148,10 @@ def accessories():
 
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
