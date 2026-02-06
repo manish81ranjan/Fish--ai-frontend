a2faee5afb3d6bd4e39e3dfba29cdb6b51af8070
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-06 22:46:22 +0530

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,9 @@
-from extensions import db
+from backend.extensions import db
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(255))
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
