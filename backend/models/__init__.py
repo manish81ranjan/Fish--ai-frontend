551554b4c16085e063289ea44a8fadb5088fc2b3
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-10 19:43:46 +0530

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -10,11 +10,12 @@
 from ..extensions import mongo
 
 # Example: collections
-users_collection = mongo.db.users
-products_collection = mongo.db.products
-carts_collection = mongo.db.carts
-orders_collection = mongo.db.orders
-accessories_collection = mongo.db.accessories
-accessory_reviews_collection = mongo.db.accessory_reviews
+users_collection = mongo.db.user
+products_collection = mongo.db.product
+carts_collection = mongo.db.cart
+orders_collection = mongo.db.order
+accessories_collection = mongo.db.accessory
+accessory_reviews_collection = mongo.db.accessory_review
+
 
 
