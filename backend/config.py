323b5b84e9b33678d3f4e11072305343dbf0c7af
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-10 22:06:26 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,10 +7,11 @@ class Config:
     MONGO_USER = quote_plus(os.getenv("MONGO_USER"))
     MONGO_PASSWORD = quote_plus(os.getenv("MONGO_PASSWORD"))
     MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")  # cluster0.xxxxx.mongodb.net
-    MONGO_DB = os.getenv("MONGO_DB", "auronox")
+    MONGO_DB = os.getenv("MONGO_DB", "Fish--ai-frontend")
 
     MONGO_URI = (
         f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}"
         f"@{MONGO_CLUSTER}/{MONGO_DB}"
         f"?retryWrites=true&w=majority"
     )
+
