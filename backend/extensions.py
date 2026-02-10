a1af670410d1ff402db622802ac2300b2e2334e2
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-10 19:31:17 +0530

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,7 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 
-db = SQLAlchemy()
+mongo = PyMongo()
+bcrypt = Bcrypt()
 jwt = JWTManager()
-cors = CORS()
