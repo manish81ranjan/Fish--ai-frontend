3be02b19d59f2d8cb8dabcdb7be1f0e14ddde96d
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-10 19:08:42 +0530

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -33,9 +33,18 @@
 # from .order import Order
 # from .payment import Payment
 
-from extensions import db
-from .product import Product
+# from extensions import db
+# from .product import Product
+# from .user import User
+# from .cart import Cart
+# from .order import Order
+# from .accessory import Accessory
+# from .accessory_review import AccessoryReview
+
+from ..extensions import db
+
 from .user import User
+from .product import Product
 from .cart import Cart
 from .order import Order
 from .accessory import Accessory
